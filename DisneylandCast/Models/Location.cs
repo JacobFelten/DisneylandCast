9e39f85361a5e8b12bf353f95b47ddd2fba40e5a
SherlockJake <7thejake7@gmail.com>
2019-10-14 18:57:01 -0700

    Added Location and Person models

---
+++ DisneylandCast/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisneylandCast.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public string Link { get; set; }
+    }
+}
